759f57ef1992cad1c0ad794361f9b37291b0849d
Iris <iris.farfan.c@gmail.com>
2016-05-19 04:12:22 -0500

    Serializacion completada, Leer modificado

--- PRACTICA_MANEJOARCHIVOS/Classes/Persona.cs
+++ PRACTICA_MANEJOARCHIVOS/Classes/Persona.cs
@@ -11,17 +11,23 @@ namespace PRACTICA_MANEJOARCHIVOS.Classes
     public class Persona
     {
         [XmlAttribute]
+        [SoapAttribute]
         public string Nombre { get; set; }
         [XmlAttribute]
+        [SoapAttribute]
         public string Apellidos { get; set; }
         [XmlAttribute]
+        [SoapAttribute]
         public string Direccion { get; set; }
         [XmlAttribute]
+        [SoapAttribute]
         public string Edad { get; set; }
         [XmlAttribute(DataType = "date")]
+        [SoapAttribute(DataType = "date")]
         public DateTime FechaNacimiento { get; set; }
 
         [XmlArray]
+        [SoapIgnore]
         public List<Documento> Documentos { get; set; }
 
     }
